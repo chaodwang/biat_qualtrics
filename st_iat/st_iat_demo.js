04b5ce409bc01b32c1e69361503fbef20c135cbc
chaodwang <cwang158@illinois.edu>
2021-07-28 23:33:37 +0800

    Update st_iat_demo.js

--- st_iat/st_iat_demo.js
+++ st_iat/st_iat_demo.js
@@ -5,7 +5,7 @@ define(['pipAPI', 'https://chaodwang.github.io/iat_qualtrics/st_iat/st_iat_full_
 		  category : { 
 		    name : 'china', //Will appear in the data.
 		    title : {
-		      media : {word : '有关中国的'}, //Name of the category presented in the task.
+		      media : {word : '中国'}, //Name of the category presented in the task.
 		      css : {color:'#31b404','font-size':'2em'}, //Style of the category title.
 		      height : 7 //Used to position the "Or" in the combined block.
 		    }, 
@@ -23,7 +23,7 @@ define(['pipAPI', 'https://chaodwang.github.io/iat_qualtrics/st_iat/st_iat_full_
 
   		attribute1 : 
 			{
-			name : 'Unpleasant', //Attribute label
+			name : 'unpleasant', //Attribute label
 			title : {
 				media : {word : '贬义词'}, //Name of the category presented in the task.
 				css : {color:'#31b404','font-size':'2em'}, //Style of the category title.
@@ -42,7 +42,7 @@ define(['pipAPI', 'https://chaodwang.github.io/iat_qualtrics/st_iat/st_iat_full_
 			},
 		attribute2 : 
 			{
-			name : 'Pleasant', //Attribute label
+			name : 'pleasant', //Attribute label
 			title : {
 				media : {word : '褒义词'}, //Name of the category presented in the task.
 				css : {color:'#31b404','font-size':'2em'}, //Style of the category title.
@@ -60,8 +60,5 @@ define(['pipAPI', 'https://chaodwang.github.io/iat_qualtrics/st_iat/st_iat_full_
 			css : {color:'#31b404','font-size':'3em'}
 			},
 
-  base_url : {//Where are your images at?
-    image : 'https://baranan.github.io/minno-tasks/images/'
-  }}
-  );
+});
   });
