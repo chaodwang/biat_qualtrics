3f34d7f5f50db8b9975af0aa18e4183e21f65eb6
chaodwang <cwang158@illinois.edu>
2021-07-30 11:53:43 +0800

    Update st_iat_demo.js

--- st_iat/st_iat_demo.js
+++ st_iat/st_iat_demo.js
@@ -6,16 +6,16 @@ define(['pipAPI', 'https://cdn.jsdelivr.net/gh/chaodwang/iat_qualtrics/st_iat/st
 		    name : '科技', //Will appear in the data.
 		    title : {
 		      media : {word : '科技'}, //Name of the category presented in the task.
-		      css : {color:'#31b404','font-size':'2em'}, //Style of the category title.
+		      css : {color:'#000080','font-size':'2em'}, //Style of the category title.
 		      height : 7 //Used to position the "Or" in the combined block.
 		    }, 
 		    media : [ //Stimuli content as PIP's media objects
-    		        {word : '科学'}, 
-    			{word : '技术'}, 
-    			{word : '算法'}, 
-    			{word : '智能'}, 
-    			{word : '交互'},
-			{word : '科技'}
+    		    {word : '高能物理'}, 
+    			{word : '基因编辑'}, 
+    			{word : '卷积计算'}, 
+    			{word : '无人驾驶'}, 
+    			{word : '量子计算'},
+				{word : '智能家居'}
 		    ], 
 		    //Stimulus css (style)
 		    css : {color:'#000000','font-size':'3em'}
@@ -26,16 +26,16 @@ define(['pipAPI', 'https://cdn.jsdelivr.net/gh/chaodwang/iat_qualtrics/st_iat/st
 			name : '贬义词', //Attribute label
 			title : {
 				media : {word : '贬义词'}, //Name of the category presented in the task.
-				css : {color:'#31b404','font-size':'2em'}, //Style of the category title.
+				css : {color:'#000080','font-size':'2em'}, //Style of the category title.
 				height : 7 //Used to position the "Or" in the combined block.
 			}, 
 			media : [ //Stimuli
-				{word: '愚蠢'},
-				{word: '笨拙'},
-				{word: '肮脏'},
-				{word: '糟糕'},
-				{word: '恐怖'},
-				{word: '堕落'}
+				{word: '愚蠢至极'},
+				{word: '笨口拙舌'},
+				{word: '藏污纳垢'},
+				{word: '千疮百孔'},
+				{word: '惶恐不安'},
+				{word: '自甘堕落'}
 			], 
 			//Can change color and size of the targets here.
 			css : {color:'#000000','font-size':'3em'}
@@ -45,16 +45,16 @@ define(['pipAPI', 'https://cdn.jsdelivr.net/gh/chaodwang/iat_qualtrics/st_iat/st
 			name : '褒义词', //Attribute label
 			title : {
 				media : {word : '褒义词'}, //Name of the category presented in the task.
-				css : {color:'#31b404','font-size':'2em'}, //Style of the category title.
+				css : {color:'#000080','font-size':'2em'}, //Style of the category title.
 				height : 7 //Used to position the "Or" in the combined block.
 			}, 
 			media : [ //Stimuli
-				{word: '聪明'},
-				{word: '吉祥'},
-				{word: '成功'},
-				{word: '开心'},
-				{word: '荣耀'},
-				{word: '勤奋'}
+				{word: '聪明伶俐'},
+				{word: '吉祥如意'},
+				{word: '大获成功'},
+				{word: '开怀大笑'},
+				{word: '高风亮节'},
+				{word: '勤学苦练'}
 			], 
 			//Can change color and size of the targets here.
 			css : {color:'#000000','font-size':'3em'}
